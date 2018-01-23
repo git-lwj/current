b749e8906c9169bed51f4447e4b9339a7b6ff6b3
梁文剑 <liangwj9@ziroom.com>
2018-01-23 16:01:37 +0800

    init

---
+++ src/main/java/com/lwj/study/current/CurrentApplication.java
@@ -0,0 +1,12 @@
+package com.lwj.study.current;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrentApplication.class, args);
+	}
+}
